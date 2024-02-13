2d3fc170f0463dc35f511c2f89cc735e90b27b73
TurboMindStudio <shivamchoudhari07@gmail.com>
2024-02-13 12:43:06 +0530

    player controller

---
+++ Assets/Scipts/Interactables/PlayerInteract.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+public class PlayerInteract : MonoBehaviour
+{
+    public Camera cam;
+    public LayerMask mask;
+    [SerializeField] float distance;
+    private PlayerUi playerUi;
+   
+    private void Start()
+    {
+        playerUi = GetComponent<PlayerUi>();
+       
+    }
+
+
+    private void Update()
+    {
+        playerUi.UpdateText(string.Empty);
+        Ray ray=new Ray(cam.transform.position, cam.transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * distance);
+        RaycastHit hit;
+       if (Physics.Raycast(ray, out hit, distance, mask))
+        {
+            if(hit.collider.GetComponent<Interactable>()!=null)
+            {
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                playerUi.UpdateText(interactable.promptMsg);
+
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    interactable.baseInteract();
+                }
+               
+            }
+        }
+        
+    }
+}
