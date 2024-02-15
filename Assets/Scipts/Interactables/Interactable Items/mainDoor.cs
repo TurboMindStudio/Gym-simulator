8a9aa1dcd71673aca792644dc69a7a014286a0d7
TurboMindStudio <shivamchoudhari07@gmail.com>
2024-02-15 10:05:18 +0530

    interactable

--- Assets/Scipts/Interactables/Interactable Items/mainDoor.cs
+++ Assets/Scipts/Interactables/Interactable Items/mainDoor.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class mainDoor : Interactable
 {
+    public Animator animator;
+    bool doorOpen;
     protected override void Interact()
     {
-        
+        doorOpen = !doorOpen;
+        if (doorOpen)
+        {
+            animator.SetBool("DoorOpen", true);
+        }
+        else if (!doorOpen)
+        {
+            animator.SetBool("DoorOpen", false);
+        }
     }
 }
--- Assets/Scipts/Interactables/PlayerInteract.cs
+++ Assets/Scipts/Interactables/PlayerInteract.cs
@@ -9,7 +9,8 @@ public class PlayerInteract : MonoBehaviour
     public LayerMask mask;
     [SerializeField] float distance;
     private PlayerUi playerUi;
-   
+    private RaycastHit hit;
+    public GameObject InteractableButton;
     private void Start()
     {
         playerUi = GetComponent<PlayerUi>();
@@ -29,14 +30,42 @@ public class PlayerInteract : MonoBehaviour
             {
                 Interactable interactable = hit.collider.GetComponent<Interactable>();
                 playerUi.UpdateText(interactable.promptMsg);
-
-                if(Input.GetKeyDown(KeyCode.E))
+                InteractableButton.SetActive(true);
+                //Windows
+                if (Input.GetKeyDown(KeyCode.E))
                 {
                     interactable.baseInteract();
                 }
                
             }
         }
+        else
+        {
+            InteractableButton.SetActive(false);
+        }
+
+    }
+
+    public void Interactable()
+    {
+
+        playerUi.UpdateText(string.Empty);
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * distance);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, distance, mask))
+        {
+            if (hit.collider.GetComponent<Interactable>() != null)
+            {
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                playerUi.UpdateText(interactable.promptMsg);
+                
+                interactable.baseInteract();
+                
+
+            }
+        }
         
+
     }
 }
